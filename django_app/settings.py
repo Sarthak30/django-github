6dbb92da4a1cdab4421cf58611d9a5b8a1aafd1d
Sarthak30 <sarthaknsit@gmail.com>
2016-01-23 22:06:27 +0530

    Complete

--- django_app/settings.py
+++ django_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#Path to template file
+TEAMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
